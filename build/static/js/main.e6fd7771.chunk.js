8df3a086ce9be663016ee9e707fd112164ffbe6e
Kay Putra <kaspr.works@gmail.com>
2021-09-25 14:25:09 +1000

    my first commit

---
+++ build/static/js/main.e6fd7771.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-complete-guide"]=this["webpackJsonpreact-complete-guide"]||[]).push([[0],{10:function(e,t,c){"use strict";c.r(t);var n=c(3),r=c.n(n),s=(c(8),c(0));var i=function(){return Object(s.jsx)("h2",{children:"Expense item!"})};var o=function(){return Object(s.jsxs)("div",{children:[Object(s.jsx)("h2",{children:"Let's get started!"}),Object(s.jsx)(i,{})]})};r.a.render(Object(s.jsx)(o,{}),document.getElementById("root"))},8:function(e,t,c){}},[[10,1,2]]]);
+//# sourceMappingURL=main.e6fd7771.chunk.js.map
